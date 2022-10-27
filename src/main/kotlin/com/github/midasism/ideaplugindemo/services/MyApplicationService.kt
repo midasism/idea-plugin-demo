6ecbf6a9cd14713f5eccc877c2bf7ebb3075bd62
GitHub Action <action@github.com>
2022-10-27 07:33:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/midasism/ideaplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/midasism/ideaplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.midasism.ideaplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.midasism.ideaplugindemo.MyBundle
 
 class MyApplicationService {
 
